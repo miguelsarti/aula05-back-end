c0a7f137f36793299a7c840d275211fac5de8781
miguelsarti <miguelsarti.tds2.senai@gmail.com>
2024-08-29 10:50:55 -0300

    feat: add some requests

--- src/server.js
+++ src/server.js
@@ -8,12 +8,60 @@ const serverPort = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 
+const emocoes = [
+  {
+    id: 1,
+    nome: "Nojinho",
+    cor: "Verde"
+  },
+
+  {
+    id: 2,
+    nome: "Alegria",
+    cor: "Amarelo"
+  },
+
+  {
+    id: 3,
+    nome: "Raiva",
+    cor: "Vermelho"
+  },
+] 
+
+const personagens = [
+  {
+    id: 100,
+    nome: "Totoro",
+    vivo: true
+  },
+
+  {
+    id: 101,
+    nome: "David Martinez",
+    vivo: false
+  },
+
+  {
+    id: 102,
+    nome: "Batman",
+    vivo: true  
+  }
+]
+
 app.get("/", (req, res) => {
-    return res.status(200).json({ message: "Hello, World!" });
+    return res.status(200).send({ message: "Hello, World!" });
+  });
+
+  app.get("/emocoes", (req, res) => {
+    return res.status(200).send(emocoes);
   });
   
 app.get("/2tds2", (req, res) => {
-    return res.status(200).json({ message: "Hello, World!" });
+    return res.status(200).send({ message: "Hello, World!" });
+  });
+
+  app.get("/personagens", (req, res) => {
+    return res.status(200).send(personagens);
   });
 
 app.listen(serverPort, () => {
