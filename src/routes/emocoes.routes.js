4bf26a641c2063f41fe51f1fd24f392ed0d13cb0
miguelsarti <miguelsarti.tds2.senai@gmail.com>
2024-09-05 17:18:58 -0300

    feat: create get by id method

--- src/routes/emocoes.routes.js
+++ src/routes/emocoes.routes.js
@@ -20,23 +20,46 @@ let emocoes = [
       nome: "Raiva",
       cor: "Vermelho"
     },
-  ] 
+  ];
+
+// Rota para buscar todas as emoções]
 
 emocoesRoutes.get("/", (req, res) => {
     return res.status(200).send(emocoes)
-})
+});
+
+// Rota para criar uma nova emoção
 
 emocoesRoutes.post("/", (req, res) => {
-    const {nome, cor} = req.body
+    const {nome, cor} = req.body;
     const newEmotion = {
     id: emocoes.length + 1,
     nome: nome,
     cor: cor
     }
-    emocoes.push(newEmotion);
 
+    emocoes.push(newEmotion);
     return res.status(201).send(emocoes);
 });
 
-export default emocoesRoutes
+// Rota para buscar uma emoção pelo id
 
+emocoesRoutes.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  //console.log(id);
+  const emocao = emocoes.find((emotion) => emotion.id == id )
+
+  if (!emocao) {
+    return res.status(404).send({
+      message: "Emoção não encontrada!",
+    });
+  }
+
+  return res.status(200).send({
+    message: "Emoção encontrada!",
+    emocao,
+  });
+});
+
+export default emocoesRoutes;
